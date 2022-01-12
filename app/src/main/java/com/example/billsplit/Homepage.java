0836af8a87789cae2d0bba2f78532788665ad0d4
SkywalkerSoumya <shadowfighter56@gmail.com>
2022-01-12 20:22:41 +0530

    Homescreen funtional btn and add_grp btn are added and minor changes in nav_bar drawer.

--- app/src/main/java/com/example/billsplit/Homepage.java
+++ app/src/main/java/com/example/billsplit/Homepage.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 
+import com.google.android.gms.cast.framework.media.ImagePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class Homepage extends AppCompatActivity {
@@ -16,11 +23,10 @@ public class Homepage extends AppCompatActivity {
     DrawerLayout drawerLayout;
     NavigationView navView;
     Toolbar toolbar;
+    //ImageView profilePic;
+    //ImageButton changeProPicButton;
 
-    //EditText editUserName;
-
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +35,7 @@ public class Homepage extends AppCompatActivity {
         // code for sideview navigation bar
         drawerLayout = findViewById(R.id.navbarlayout);
         navView = findViewById(R.id.navview);
+
         toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
@@ -38,8 +45,47 @@ public class Homepage extends AppCompatActivity {
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        // code for set User Name in navigation bar header
-        //editUserName = (EditText) findViewById(R.id.editUserName);
+        //settle up button function
+        Button settleup_btn = (Button) findViewById(R.id.settleUpBtn);
+
+        settleup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(Homepage.this,SettleUpPage.class);
+                startActivity(intent1);
+            }
+        });
+
+        // Expenses button function
+        Button expenses_btn = (Button) findViewById(R.id.expensesBtn);
+
+        expenses_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(Homepage.this,Expenses_Details.class);
+                startActivity(intent2);
+            }
+        });
+
+
+
+        // code for changing profile picture  ---> CHANGES NEEDED!
+
+       /* profilePic = findViewById(R.id.profileImageView);
+        changeProPicButton = findViewById(R.id.editProfilePicImageButton);
+
+        changeProPicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.Companion.with(Homepage.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .cropOval()	    		//Allow dimmed layer to have a circle inside
+                        .cropFreeStyle()	    //Let the user to resize crop bounds
+                        .galleryOnly()          //We have to define what image provider we want to use
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .createIntent();
+            }
+        });*/
 
     }
 
