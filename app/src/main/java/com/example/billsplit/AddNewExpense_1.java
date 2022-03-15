aa7ff74c587aad5beab88c34df68a434c8ca4eb9
SoumavaSeal <sealsoumava@gmail.com>
2022-03-15 19:38:08 +0530

    new expense UI added.

---
+++ app/src/main/java/com/example/billsplit/AddNewExpense_1.java
@@ -0,0 +1,14 @@
+package com.example.billsplit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewExpense_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_expense_1);
+    }
+}
